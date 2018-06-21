80115ee68c5fe0a888451f3b5a1293938b2cc516
fiona-wong <feeonawong@gmail.com>
2018-06-21 14:49:49 -0700

    fixed bug

--- schedule_appt.py
+++ schedule_appt.py
@@ -1,8 +1,13 @@
 import os.path
+import string
 from splinter import Browser
 from datetime import datetime
 
 executable_path = {'executable_path':'/usr/local/bin/chromedriver'}
+
+def compare(string1, string2):
+    remove = string.whitespace
+    return string1.translate(None, remove) == string2.translate(None, remove)
 
 with Browser("chrome", **executable_path) as browser:
     # visit url
@@ -22,7 +27,7 @@ with Browser("chrome", **executable_path) as browser:
     tel_area = "XXX"
     tel_prefix = "XXX"
     tel_suffix = "XXXX"
-    email = "email@gmail.com"
+    email = "something@gmail.com"
     # Make sure you have a future date set in the appt.txt file
     current_appt_time = None
     with open(apptfile, 'r') as myfile:
@@ -53,6 +58,7 @@ with Browser("chrome", **executable_path) as browser:
     if browser.is_element_present_by_id('formId_1', 20):
         appt_xpath = '//div[@class="r-table col-xs-12"]'
         appt_table = browser.find_by_xpath(appt_xpath).first.text
+        print appt_table
         appt_time_str = appt_table.split('\n')[5]
         current_appt_datetime = datetime.strptime(current_appt_time, '%A, %B %d, %Y at %I:%M %p')
         new_appt_datetime_object = datetime.strptime(appt_time_str, '%A, %B %d, %Y at %I:%M %p')
@@ -60,6 +66,22 @@ with Browser("chrome", **executable_path) as browser:
         if is_new_appttime_earlier:
             continue_xpath = "//a[contains(text(), 'Continue')]"
             browser.find_by_xpath(continue_xpath).click()
+            # if you already have a prior appt it goes here to confirm
+            if browser.is_element_present_by_id("keepApptDate", 20):
+                print browser.find_by_value("Confirm New Appointment")
+                browser.find_by_value("Confirm New Appointment").click()
+                if browser.is_element_present_by_value("Continue", 20):
+                    browser.choose("notificationMethod", "EMAIL")
+                    browser.find_by_name("notify_email").fill(email)
+                    browser.find_by_name("notify_email_confirm").fill(email)
+                    browser.find_by_value("Continue").click()
+                    if browser.is_element_present_by_value("Confirm", 20):
+                        browser.find_by_value("Confirm").click()
+                        file = open(apptfile, 'w')
+                        file.write(appt_time_str)
+                        file.close()
+                        print("Successfully booked your new appointment at {}".format(appt_time_str))
+                        quit()
             if browser.is_element_present_by_name('ApptForm', 20):
                 # if you don't have a prior appt it goes here to confirm
                 browser.choose("notificationMethod", "SMS")
@@ -70,28 +92,15 @@ with Browser("chrome", **executable_path) as browser:
                 browser.find_by_name("notify_smsTelPrefix_confirm").fill(tel_prefix)
                 browser.find_by_name("notify_smsTelSuffix_confirm").fill(tel_suffix)
                 browser.find_by_value("Continue").click()
+
                 if browser.is_element_present_by_value("Confirm", 20):
                     browser.find_by_value("Confirm").click()
-                    file = open(apptfiledir, 'w')
+                    file = open(apptfile, 'w')
                     file.write(appt_time_str)
                     file.close()
                     print("Successfully booked your appointment at {}".format(appt_time_str))
                     quit()
-                # if you already have a prior appt it goes here to confirm
-                if browser.is_element_present_by_value("Confirm New Appointment", 20):
-                    browser.find_by_value("Confirm New Appointment").click()
-                    if browser.is_element_present_by_value("Continue", 20):
-                        browser.choose("notificationMethod", "EMAIL")
-                        browser.find_by_name("notify_email").fill(email)
-                        browser.find_by_name("notify_email_confirm").fill(email)
-                        browser.find_by_value("Continue").click()
-                        if browser.is_element_present_by_value("Confirm", 20):
-                            browser.find_by_value("Confirm").click()
-                            file = open(apptfiledir, 'w')
-                            file.write(appt_time_str)
-                            file.close()
-                            print("Successfully booked your new appointment at {}".format(appt_time_str))
-                            quit()
+
         else:
             print("Appointment time is later than the one you currently have :(")
             quit()
